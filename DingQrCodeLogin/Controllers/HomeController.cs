49e98fbcfd91dbc226a0aaa46fa9b88895546ea4
Jelly <jelly.liut@gmail.com>
2019-10-24 15:22:16 +0800

    增加说明

--- DingQrCodeLogin/Controllers/HomeController.cs
+++ DingQrCodeLogin/Controllers/HomeController.cs
@@ -45,10 +45,7 @@ namespace DingQrCodeLogin.Controllers
                 DefaultDingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/sns/getuserinfo_bycode");
                 OapiSnsGetuserinfoBycodeRequest req = new OapiSnsGetuserinfoBycodeRequest();
                 req.TmpAuthCode = code;
-                response = client.Execute(req, qrAppId, qrAppSecret);
-
-
-
+                response = client.Execute(req, qrAppId, qrAppSecret); 
 
                 //获取到response后就可以进行自己的登录业务处理了
 
